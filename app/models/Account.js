25ddd891ab81d7d75327b55f62b0dd5a20461d1e
Yogayshri <141368111+yogayshri07@users.noreply.github.com>
2023-08-25 14:00:25 -0400

    added amad

--- app/models/Account.js
+++ app/models/Account.js
@@ -1,43 +1,46 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const AccountSchema = new Schema({
+const AccountSchema = new Schema(
+  {
     firstName: String,
     lastName: String,
     mobile: String,
     careOf: {
-        type: String,
-        enum: ['S/O', 'W/O', 'D/O']
+      type: String,
+      enum: ["S/O", "W/O", "D/O"],
     },
     careOfName: String,
     address: Object,
     profilePic: Object,
     type: {
-        type: String,
-        enum: ['kisan', 'staff', 'others']
+      type: String,
+      enum: ["kisan", "staff", "others"],
     },
     amad: {
-        type: Schema.Types.ObjectId,
-        ref: 'Amad',
-        index: true
+      type: Schema.Types.ObjectId,
+      ref: "Amad",
+      index: true,
     },
-    bankAccount: [{
+    bankAccount: [
+      {
         type: Schema.Types.ObjectId,
-        ref: 'BankAccount',
-        index: true
-    }],
-
-
+        ref: "BankAccount",
+        index: true,
+      },
+    ],
     isDeleted: { type: Boolean, default: false },
     createdBy: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     updatedBy: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 const AccountObj = mongoose.model('Account', AccountSchema);
 module.exports = AccountObj;
--- app/models/Company.js
+++ app/models/Company.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CompanySchema = new Schema({
-  organization: {
-    type: Schema.Types.ObjectId,
-    ref: 'Organization',
-    index: true
+const CompanySchema = new Schema(
+  {
+    companyCode: { type: String, unique: true, index: true },
+    organization: {
+      type: Schema.Types.ObjectId,
+      ref: "Organization",
+      index: true,
+    },
+    name: String,
+    email: String,
+    phone: String,
+
+    industry: String,
+    website: String,
+    address: Object,
+    logo: Object,
+    isDeleted: { type: Boolean, default: false },
   },
-  name: String,
-  email: String,
-  phone: String,
-  companyCode: { type: String, unique: true, index: true },
-  industry: String,
-  website: String,
-  address: Object,
-  logo: Object,
-  isDeleted: { type: Boolean, default: false }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const ComapanyObj = mongoose.model('Company', CompanySchema);
 module.exports = ComapanyObj;
