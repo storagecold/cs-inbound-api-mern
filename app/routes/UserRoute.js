5655a01ebc111bea729cf599fb5564268c449047
Pankaj Kumar <122406409+pankajsmallarc@users.noreply.github.com>
2023-08-27 18:50:24 +0530

    login fixed

--- app/routes/UserRoute.js
+++ app/routes/UserRoute.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const UserAuthCtrl = require('../controllers/UserController');
 const checkAuth = require("../middleware/check-auth");
 
-router.post("/users", UserAuthCtrl.Login);
-router.post("/users", UserAuthCtrl.SignUp);
-router.post("/users", UserAuthCtrl.ForgotPassword);
-router.post("/users", UserAuthCtrl.ResetPassword);
+router.post("/users/login", UserAuthCtrl.Login);
+router.post("/users/singnup", UserAuthCtrl.SignUp);
+router.post("/users/forgetPassword", UserAuthCtrl.ForgotPassword);
+router.post("/users/resetpassword", UserAuthCtrl.ResetPassword);
 
-router.patch('/users', checkAuth, UserAuthCtrl.ChangePassword);
-router.post("/users", checkAuth, UserAuthCtrl.GetProfile);
-router.patch("/users", checkAuth, UserAuthCtrl.UpdateProfile);
+router.patch('/users/changePassword', checkAuth, UserAuthCtrl.ChangePassword);
+router.post("/users/getProfile", checkAuth, UserAuthCtrl.GetProfile);
+router.patch("/users/updateProfile", checkAuth, UserAuthCtrl.UpdateProfile);
 
 module.exports = router;
