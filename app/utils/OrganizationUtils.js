59876e6d15c9afafa09f1a8e17e278892f925e2c
Yogayshri <141368111+yogayshri07@users.noreply.github.com>
2023-09-02 21:16:47 -0400

    Revert "Revert "refactored test cases""
    This reverts commit 6710a82f353fbc72506dbc681ae8254de115718f.

--- app/utils/OrganizationUtils.js
+++ app/utils/OrganizationUtils.js
@@ -8,30 +8,34 @@ module.exports = {
 
     OrganizationValidate: function (body) {
         const schema = Joi.object({
-            name: Joi.string().max(30).required(),
-            email: Joi.string().email().required(),
-            phone: Joi.number().max(12),
-            mobile: Joi.number().max(10).required(),
-            industry: Joi.string().max(15).required(),
-            website: Joi.string(),
-            address: Joi.object({
-              addressLineOne: Joi.string(),
-              city: Joi.string(),
-              district: Joi.string(),
-              state: Joi.string(),
-              pinCode: Joi.string()
-            }).required(),
-            logo: Joi.object({
-              originalName: Joi.string(),
-              location: Joi.string(),
-              key: Joi.string()
-            }),
-            owner: Joi.array().items(Joi.string().required()),
-            isDeleted: Joi.boolean(),
-            deletedAt: Joi.date().allow(null),
-            createdBy: Joi.string(),
-            updatedBy: Joi.string() 
-          });
+          name: Joi.string().max(30).required(),
+          email: Joi.string().email().required(),
+          phone: Joi.string()
+            .regex(/^\d{1,12}$/)
+            .required(),
+          mobile: Joi.string()
+            .regex(/^\d{1,10}$/)
+            .required(),
+          industry: Joi.string().max(15).required(),
+          website: Joi.string(),
+          address: Joi.object({
+            addressLineOne: Joi.string(),
+            city: Joi.string(),
+            district: Joi.string(),
+            state: Joi.string(),
+            pinCode: Joi.string(),
+          }).required(),
+          logo: Joi.object({
+            originalName: Joi.string(),
+            location: Joi.string(),
+            key: Joi.string(),
+          }),
+          owner: Joi.array().items(Joi.string().required()),
+          isDeleted: Joi.boolean(),
+          deletedAt: Joi.date().allow(null),
+          createdBy: Joi.string(),
+          updatedBy: Joi.string(),
+        });
 
 
         return schema.validate(body)
